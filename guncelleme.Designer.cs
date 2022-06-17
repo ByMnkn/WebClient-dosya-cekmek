5f81efa1addc69bb7288cdf1fcb5442f0fd0a5d5
Mustafa <bymnkn_@hotmail.com>
2022-06-17 14:25:24 +0300

    WebClient dosya çekmek

---
+++ guncelleme.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Guncelleme
+{
+    partial class guncelleme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(guncelleme));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ilerlemedurum_label = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.zaman_label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(15, 62);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(280, 41);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // ilerlemedurum_label
+            // 
+            this.ilerlemedurum_label.AutoSize = true;
+            this.ilerlemedurum_label.Location = new System.Drawing.Point(12, 13);
+            this.ilerlemedurum_label.Name = "ilerlemedurum_label";
+            this.ilerlemedurum_label.Size = new System.Drawing.Size(42, 13);
+            this.ilerlemedurum_label.TabIndex = 1;
+            this.ilerlemedurum_label.Text = "ilerleme";
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // zaman_label
+            // 
+            this.zaman_label.AutoSize = true;
+            this.zaman_label.Location = new System.Drawing.Point(12, 35);
+            this.zaman_label.Name = "zaman_label";
+            this.zaman_label.Size = new System.Drawing.Size(38, 13);
+            this.zaman_label.TabIndex = 2;
+            this.zaman_label.Text = "zaman";
+            // 
+            // guncelleme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 113);
+            this.Controls.Add(this.zaman_label);
+            this.Controls.Add(this.ilerlemedurum_label);
+            this.Controls.Add(this.progressBar1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "guncelleme";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Güncelleme";
+            this.Load += new System.EventHandler(this.Guncel_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label ilerlemedurum_label;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label zaman_label;
+    }
+}
---
+++ guncelleme.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Aşağıdaki referansı ekliyoruz.
+using System.Net;
+using System.Diagnostics;
+
+namespace Guncelleme
+{
+    public partial class guncelleme : Form
+    {
+        private string destPath = Application.StartupPath + "\\Panel.exe"; //güncellenecek dosyamızın adı
+        private string GuncelDosya = "http://localhost/app/Panel.exe"; //Sunucudan çekeceğimiz dosyanın adresi
+        private string DosyaAdi;
+        private int saniye = 5;
+
+        public guncelleme()
+        {
+            InitializeComponent();
+        }
+
+        private void Guncel_Load(object sender, EventArgs e)
+        {
+            timer1.Start(); //Load eventinde timeri aktif ediyoruz.
+            zaman_label.Text = "";
+        }
+        private void timer1_Tick(object sender, EventArgs e) //Timer tick kısmı
+        {
+            timer1.Enabled = false; //Timerin bir kere çalışmasını istediğimiz için timeri pasif yapıyoruz.
+            WebClient webClient = new WebClient(); //Webclient tanımlıyoruz.
+            webClient.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed); //Webclient içinde webClient.DownloadFileCompleted olayı tanımlıyoruz. Yani dosya yüklenmesi bitince bu kısım tetiklenecek.
+            webClient.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);//Webclient içinde webClient.DownloadProgressChanged olayı tanımlıyoruz. Dosyanın her bir kilobyte ı indiğinde bu olay tetiklenecek.
+            webClient.DownloadFileAsync(new Uri(GuncelDosya), destPath); //En yukarıda tanımladıgımız GuncelDosya değişkeninde ki linki indirmeye başlıyoruz.
+
+            //Bu kısımda ise indireceğimiz dosyanın sadece adını ve uzantısını çekiyoruz. Yani GuncelDosya değişkeninde ki linkin gereksiz kısımlarını silip sadece dosya adını ve uzantısını alıyoruz. 
+            string DosyaAdiUrlAdresi = GuncelDosya;
+            int karaktersayisi = DosyaAdiUrlAdresi.LastIndexOf('/');
+            DosyaAdi = DosyaAdiUrlAdresi.Remove(0, karaktersayisi + 1);
+        }
+
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e) //Webclient kısmında tanımladığımız olay tetiklendiğinde bu kısım çalışacak.
+        {
+            progressBar1.Value = e.ProgressPercentage; //progressbarın ilerlemesini sağlıyoruz.
+            ilerlemedurum_label.Text = DosyaAdi + " (%" + e.ProgressPercentage.ToString() + ")"; //Yüklenen kısmı yüzde olarak gösteriyoruz.
+        }
+
+        private void Completed(object sender, AsyncCompletedEventArgs e) //Webclient kısmında tanımladığımız olay tetiklendiğinde bu kısım çalışacak.
+        {
+            timer2.Interval = 1000;
+            timer2.Start();
+            ilerlemedurum_label.Text = "Güncelleme Tamamlandı!"; // Yükleme bitti
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            zaman_label.Text = Convert.ToString(saniye) + " saniye sonra yeniden başlayacak!"; // Yükleme bitti
+            saniye = saniye - 1;
+            if(saniye == 0)
+            {
+                Process.Start(Application.StartupPath + "\\Panel.exe"); //güncellenecek dosyamızın adı
+                Process kapat;
+                kapat = Process.GetProcessesByName("Guncelleme")[0];
+                kapat.Kill();
+            }
+        }
+    }
+}
